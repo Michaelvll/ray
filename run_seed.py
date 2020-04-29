23524be90f27e1ce2668b9bce72e24520cb2d124
Zhanghao Wu <zhanghao.wu@outlook.com>
2020-04-29 22:07:11 +0800

    [minor] change run_seed config

--- run_seed.py
+++ run_seed.py
@@ -3,7 +3,10 @@ from ray.rllib.agents.seed import SeedTrainer
 tune.run(SeedTrainer, config={"env": "BreakoutNoFrameskip-v4",
                                 "num_gpus": 0,
                                 "num_workers": 0,
-                                "num_envs_per_worker": 4
+                                "num_envs_per_worker": 4,
+                                "num_cpus_per_worker": 4,
+                                "num_gpus_per_worker": 0,
+                                "remote_worker_envs": True
                                 })  # "log_level": "INFO" for verbose,
 # "eager": True for eager execution,
 # "torch": True for PyTorch
