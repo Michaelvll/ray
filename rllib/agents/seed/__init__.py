09aa4329447545888043da510247e39d81856e8b
Zhanghao Wu <zhanghao.wu@outlook.com>
2020-05-06 10:50:48 +0800

    [minor] fix seed folder

--- rllib/agents/seed/__init__.py
+++ rllib/agents/seed/__init__.py
@@ -1,6 +1,6 @@
 from ray.rllib.agents.seed.seed import SeedTrainer, DEFAULT_CONFIG
 from ray.rllib.utils import renamed_agent
 
-ImpalaAgent = renamed_agent(SeedTrainer)
+SeedAgent = renamed_agent(SeedTrainer)
 
 __all__ = ["SeedAgent", "SeedTrainer", "DEFAULT_CONFIG"]
