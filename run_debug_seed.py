d3539f8f895b6d07b3705c2576defcaf517eb5e7
Zhanghao Wu <zhanghao.wu@outlook.com>
2020-05-08 12:53:13 +0800

    [debug] change sample_async to 1 worker

--- run_debug_seed.py
+++ run_debug_seed.py
@@ -3,13 +3,13 @@ from ray.rllib.agents.seed import SeedTrainer
 tune.run(SeedTrainer, config={"env": "BreakoutNoFrameskip-v4",
                               # Configs for Resources
                               "num_gpus": 0,
-                              "num_workers": 0,
+                              "num_workers": 1,
                               "num_envs_per_worker": 5,
                               "num_cpus_per_worker": 5,
                               "num_gpus_per_worker": 0,
-                              # "remote_env_poll_size": 2,
-                              # "remote_worker_envs": True,
-                              "sample_async": True,
+                              "remote_env_poll_size": 2,
+                              "remote_worker_envs": True,
+                              # "sample_async": True,
                             #   "log_level": "INFO" # for verbose
                             })  # "log_level": "INFO" for verbose,
 # "eager": True for eager execution,
