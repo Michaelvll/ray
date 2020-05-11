f4df0f2920c035fb8ce26db15200536100095fad
Zhanghao Wu <zhanghao.wu@outlook.com>
2020-05-11 20:47:54 +0800

    [debug] run with 1 worker

--- run_gpu_seed.py
+++ run_gpu_seed.py
@@ -3,7 +3,7 @@ from ray.rllib.agents.seed import SeedTrainer
 tune.run(SeedTrainer, config={"env": "BreakoutNoFrameskip-v4",
                               # Configs for Resources
                               "num_gpus": 1,
-                              "num_workers": 0,
+                              "num_workers": 1,
                               "num_envs_per_worker": 5,
                               "num_cpus_per_worker": 5,
                               "num_gpus_per_worker": 0,
