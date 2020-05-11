b3ec42181c102a349c93b9f361310d6523620ddf
Zhanghao Wu <zhanghao.wu@outlook.com>
2020-05-11 22:27:32 +0800

    [!!! tmp debug !!!] use tf.device GPU and 1 worker

--- run_gpu_seed.py
+++ run_gpu_seed.py
@@ -6,7 +6,7 @@ tune.run(SeedTrainer, config={"env": "BreakoutNoFrameskip-v4",
                               "num_workers": 1,
                               "num_envs_per_worker": 5,
                               "num_cpus_per_worker": 5,
-                              "num_gpus_per_worker": 1,
+                              "num_gpus_per_worker": 0,
                               "remote_env_poll_size": 2,
                               "remote_worker_envs": True,
                               "sample_async": True,
@@ -15,5 +15,3 @@ tune.run(SeedTrainer, config={"env": "BreakoutNoFrameskip-v4",
                             })  # "log_level": "INFO" for verbose,
 # "eager": True for eager execution,
 # "torch": True for PyTorch
-
-# TODO: test tf.device
