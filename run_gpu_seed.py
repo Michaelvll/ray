fdc9de1a6e982c164c70a97c7a2ba79aff9a234e
Zhanghao Wu <zhanghao.wu@outlook.com>
2020-06-08 12:30:56 +0000

    [debug] CPU inference

--- run_gpu_seed.py
+++ run_gpu_seed.py
@@ -2,11 +2,11 @@ from ray import tune
 from ray.rllib.agents.seed import SeedTrainer
 tune.run(SeedTrainer, config={"env": "BreakoutNoFrameskip-v4",
                               # Configs for Resources
-                              "num_gpus": 0.5,
+                              "num_gpus": 1,
                               "num_workers": 1,
                               "num_envs_per_worker": 10,
                               "num_cpus_per_worker": 10,
-                              "num_gpus_per_worker": 0.5,
+                              "num_gpus_per_worker": 0,
                               "remote_env_poll_size": 5,
                               "remote_worker_envs": True,
                               "sample_async": True,
