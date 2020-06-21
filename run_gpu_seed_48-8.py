5c34f99b3b9936a36c6f56f817c5379ed378d8f1
Zhanghao Wu <zhanghao.wu@outlook.com>
2020-06-21 05:38:33 +0000

    [debug] 8 Workers

---
+++ run_gpu_seed_48-8.py
@@ -0,0 +1,22 @@
+from ray import tune
+from ray.rllib.agents.seed import SeedTrainer
+tune.run(SeedTrainer, config={"env": "BreakoutNoFrameskip-v4",
+                              # Configs for Resources
+                              "num_gpus": 0.1,
+                              "num_workers": 8,
+                              "num_envs_per_worker": 6,
+                              "num_cpus_per_worker": 6,
+                              "num_gpus_per_worker": 0.1,
+                              "remote_env_poll_size": 4,
+                              "remote_worker_envs": True,
+                              "sample_async": True,
+                              "rollout_fragment_length": 20,
+                              "train_batch_size": 640,
+                              "sample_max_steps": 80,
+                              # "eager": True,
+                              # "log_level": "INFO" # for verbose
+                            },
+                            local_dir="/data/ray_results"
+                            )  # "log_level": "INFO" for verbose,
+# "eager": True for eager execution,
+# "torch": True for PyTorch
